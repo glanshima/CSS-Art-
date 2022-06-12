283a80e8fdd0b361112bae39880f9e853537fb81
unknown <glanshima@gmail.com>
2022-06-12 19:57:20 +0100

    changed mouse over events on the eyes using events deligation

--- bearScript.js
+++ bearScript.js
@@ -5,6 +5,26 @@ let getTeddyHead = document.querySelector('.teddy-head');
 let teddyEyeLeft = document.querySelector('.teddy-eye-left');
 let teddyEyeRight = document.querySelector('.teddy-eye-right');
 
+closeEyesEvents('mouseover', ".teddy-eye-left", e => {
+
+    teddyEyeLeft.style.cssText = "background-color: #270303; cursor: pointer";
+    teddyEyeLeftAfter.innerHTML = ".teddy-eye-left:after {background: #270303}";
+})
+closeEyesEvents('mouseover', ".teddy-eye-right", e => {
+
+    teddyEyeRight.style.cssText = "background-color: #270303; cursor: pointer";
+    teddyEyeRightAfter.innerHTML = ".teddy-eye-right:after {background: #270303}";
+})
+
+
+
+function closeEyesEvents(type, selector, callback) {
+    document.addEventListener(type, (e) => {
+        if (e.target.matches(selector)) callback(e);
+    })
+
+}
+
 //let teddyEyeCS = window.getComputedStyle(teddyEyeLeft, "::after");
 
 //accessing pseudo element to change color
@@ -14,24 +34,13 @@ let teddyEyeRightAfter = document.head.appendChild(document.createElement("style
 
 
 
-getTeddyHead.addEventListener('mouseover', function() {
-    teddyEyeLeft.style = "background-color: #270303";
-    teddyEyeRight.style = "background-color: #270303";
-    teddyEyeLeftAfter.innerHTML = ".teddy-eye-left:after {background: #270303}";
-    teddyEyeRightAfter.innerHTML = ".teddy-eye-right:after {background: #270303}";
-
-
-
-
-})
-getTeddyHead.addEventListener('mouseout', function() {
-
-    teddyEyeLeft.style = "background-color: #fff";
-    teddyEyeRight.style = "background-color: #fff";
-    teddyEyeLeftAfter.innerHTML = ".teddy-eye-left:after {background: #000}";
-    teddyEyeRightAfter.innerHTML = ".teddy-eye-right:after {background: #000}";
-
-
+closeEyesEvents('mouseout', ".teddy-eyes", e => {
+    setTimeout(() => {
+        teddyEyeLeft.style = "background-color: #fff";
+        teddyEyeRight.style = "background-color: #fff";
+        teddyEyeLeftAfter.innerHTML = ".teddy-eye-left:after {background: #000}";
+        teddyEyeRightAfter.innerHTML = ".teddy-eye-right:after {background: #000}";
+    }, 500);
 
 })
 
